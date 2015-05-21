b67dc4117d1c0412b86bf9d1eb4ca041c4f2e07e
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2015-05-21 23:50:59 +0300

    Removing convoluted hierarchy of abstract test classes.

--- src/SoundFingerprinting.SQL.Tests/Integration/AbstractIntegrationTest.cs
+++ src/SoundFingerprinting.SQL.Tests/Integration/AbstractIntegrationTest.cs
@@ -7,13 +7,59 @@
 
     using SoundFingerprinting.Audio;
     using SoundFingerprinting.DAO;
+    using SoundFingerprinting.DAO.Data;
     using SoundFingerprinting.Data;
 
     [DeploymentItem(@"TestEnvironment\floatsamples.bin")]
     [DeploymentItem(@"TestEnvironment\Kryptonite.mp3")]
+    [DeploymentItem(@"x86", @"x86")]
+    [DeploymentItem(@"x64", @"x64")]
     [TestClass]
-    public abstract class AbstractIntegrationTest : AbstractTest
+    public abstract class AbstractIntegrationTest 
     {
+        protected const double Epsilon = 0.0001;
+
+        protected const int SampleRate = 5512;
+
+        protected const string PathToMp3 = @"Kryptonite.mp3";
+
+        protected const string PathToSamples = @"floatsamples.bin";
+
+        protected readonly bool[] GenericFingerprint = new[]
+            {
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true, true, false, true, false, true, false, true, false,
+                true, false, true, false, false, true, false, true, false, true, false, true, false, true, false, true,
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true, true, false, true, false, true, false, true, false,
+                true, false, true, false, false, true, false, true, false, true, false, true, false, true, false, true,
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true
+            };
+
+        protected readonly byte[] GenericSignature = new[]
+            {
+                (byte)1, (byte)2, (byte)3, (byte)4, (byte)5, (byte)6, (byte)7, (byte)8, (byte)9, (byte)10, (byte)11,
+                (byte)12, (byte)13, (byte)14, (byte)15, (byte)16, (byte)17, (byte)18, (byte)19, (byte)20, (byte)21,
+                (byte)22, (byte)23, (byte)24, (byte)25
+            };
+
+        protected readonly long[] GenericHashBuckets = new[]
+            {
+                1L, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 
+            };
+
+        protected void AssertTracksAreEqual(TrackData expectedTrack, TrackData actualTrack)
+        {
+            Assert.AreEqual(expectedTrack.TrackReference, actualTrack.TrackReference);
+            Assert.AreEqual(expectedTrack.Album, actualTrack.Album);
+            Assert.AreEqual(expectedTrack.Artist, actualTrack.Artist);
+            Assert.AreEqual(expectedTrack.Title, actualTrack.Title);
+            Assert.AreEqual(expectedTrack.TrackLengthSec, actualTrack.TrackLengthSec);
+            Assert.AreEqual(expectedTrack.ISRC, actualTrack.ISRC);
+            Assert.AreEqual(expectedTrack.GroupId, actualTrack.GroupId);
+        }
+
         protected void AssertHashDatasAreTheSame(IList<HashedFingerprint> firstHashDatas, IList<HashedFingerprint> secondHashDatas)
         {
             Assert.AreEqual(firstHashDatas.Count, secondHashDatas.Count);
